38fd1a4f2e13e42f538dff3c97a2dcea34ae6383
MSUResult <adarshrajput9040@gmail.com>
2025-08-06 16:11:02 +0530

    Build working

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -125,7 +125,7 @@ export default function Contact() {
               <FaMapMarkerAlt className="h-6 w-6 text-primary" />
               <div>
                 <h3 className="font-semibold">Location</h3>
-                <p className="text-secondary">San Francisco, CA</p>
+                <p className="text-secondary">Saharanpur, India</p>
               </div>
             </motion.div>
           </motion.div>
