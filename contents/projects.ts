ca427d8b7adf898e3a1a49e7ed3b07af5d369089
MSUResult <adarshrajput9040@gmail.com>
2025-11-18 22:29:13 +0530

    1st

--- contents/projects.ts
+++ contents/projects.ts
@@ -4,20 +4,21 @@ export const projects: Project[] = [
   {
     title: "Property Government Rate Finding",
     description:
-      "A full-stack platform that helps users easily find accurate government-approved property rates for their region. I built this project after noticing that most people don’t know the official land and property prices in their area, which often leads to confusion or misguidance. This website solves that by providing clear, reliable rate information with a smooth UI and fast performance",
+      "Find accurate, government-approved property rates instantly with a clean, fast UI.",
     technologies: ["Next.js", "TypeScript", "Tailwind CSS", "Stripe"],
     githubLink: "https://github.com/MSUResult/SaharanpurPropertyPrice",
     demoLink: "https://saharanpurprice.in",
     image: "/projects/real.png",
   },
   {
-    title: "Task Management App",
+    title: "Quiz Website",
     description:
-      "A collaborative task management application with real-time updates.",
-    technologies: ["React", "Node.js", "MongoDB", "Socket.io"],
-    githubLink: "Thise is private message show ",
-    demoLink: "https://github.com/MSUResult",
-    image: "/projects/task-manager.webp",
+      "A fast, interactive quiz platform with dynamic questions and instant score tracking",
+    technologies: ["React", "Nextjs", "Gemini API"],
+    githubLink: "https://github.com/MSUResult/Quiz_Expert",
+    demoLink:
+      "https://www.ksat.com/weather/2025/08/05/hot-quiet-this-week-so-when-is-our-average-first-front/",
+    image: "/projects/weather-app.png",
   },
   {
     title: "Agency Website",
@@ -29,6 +30,16 @@ export const projects: Project[] = [
     image: "/projects/portfolio-website.jpg",
   },
   {
+    title: "Task Management App",
+    description:
+      "A collaborative task management application with real-time updates.",
+    technologies: ["React", "Node.js", "MongoDB", "Socket.io"],
+    githubLink: "Thise is private message show ",
+    demoLink: "https://github.com/MSUResult",
+    image: "/projects/task-manager.webp",
+  },
+
+  {
     title: "Blog Platform",
     description:
       "A blogging platform with user authentication and markdown support.",
@@ -37,22 +48,12 @@ export const projects: Project[] = [
     demoLink: "https://demo.com",
     image: "/projects/blog-website.jpeg",
   },
-  {
-    title: "Quiz Website",
-    description:
-      "An interactive quiz platform designed to make learning fun, fast, and engaging. Users can test their knowledge across multiple categories, track scores in real time, and challenge themselves with dynamically generated questions. The website delivers a smooth, responsive experience with clean UI, instant feedback, and smart question logic—perfect for students, learners, and anyone who loves quizzes.",
-    technologies: ["React", "Nextjs", "Gemini API"],
-    githubLink: "https://github.com/MSUResult/Quiz_Expert",
-    demoLink:
-      "https://www.ksat.com/weather/2025/08/05/hot-quiet-this-week-so-when-is-our-average-first-front/",
-    image: "/projects/weather-app.png",
-  },
+
   {
     title: "Chat Application",
     description: "A real-time chat application using WebSocket technology.",
     technologies: ["React", "Node.js", "Socket.io"],
-    githubLink:
-      "https://github.com/codebucks27/Nextjs-tailwindcss-blog-template?tab=readme-ov-file",
+    githubLink: "https://github.com/MSUResult",
     demoLink: "https://create-blog-with-nextjs.vercel.app/",
     image: "/projects/chat-app.png",
   },
@@ -60,7 +61,7 @@ export const projects: Project[] = [
     title: "Recipe Finder",
     description: "A recipe search application using the Edamam API.",
     technologies: ["React", "Edamam API"],
-    githubLink: "https://github.com/Jupiter-Github/Recipe-Finder",
+    githubLink: "https://github.com/MSUResult",
     demoLink: "https://recipe-finder-rho-gilt.vercel.app/",
     image: "/projects/recipe-finder.png",
   },
@@ -68,7 +69,7 @@ export const projects: Project[] = [
     title: "Expense Tracker",
     description: "A personal finance tracker to manage expenses and income.",
     technologies: ["React", "Firebase"],
-    githubLink: "https://github.com/mayswind/ezbookkeeping?tab=readme-ov-file",
+    githubLink: "https://github.com/MSUResult",
     demoLink: "https://ezbookkeeping-demo.mayswind.net/desktop#/",
     image: "/projects/expense-tracker.webp",
   },
